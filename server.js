40f0f426a386388964f8a51e19c9b0d88be7ebec
srinidhikulkarni16 <srinidhikulkarni90@gmail.com>
2026-02-20 22:29:06 +0530

    Initial Trustivo backend setup

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Trustivo API is running 🚀" });
+});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
